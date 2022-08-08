884fe1b6ab8ce9c45136b84083dbec5e859f510d
michael <michaelglenn88@yahoo.com>
2022-08-07 20:22:01 -0400

    commit tree 1667de7222af8927269f08ee3d01cd976b33353a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,37 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do
+    # find the review using the ID
+    # delete the review
+    # send a response with the deleted review as JSON
+  rev= Review.find(params[:id])
+  rev.destroy
+  rev.to_json
+  end
+
+  post '/reviews' do
+    # find the review using the ID
+    # delete the review
+    # send a response with the deleted review as JSON
+  rev= Review.create(
+    score: params[:score],
+    comment: params[:comment],
+    game_id: params[:game_id],
+    user_id: params[:user_id]
+  )
+  rev.to_json
+  end
+
+  patch '/reviews/:id' do
+    # find the review using the ID
+    # delete the review
+    # send a response with the deleted review as JSON
+  rev= Review.find(params[:id])
+  rev.update(
+    score: params[:score],
+    comment: params[:comment]
+  )
+  rev.to_json
+  end
 end
